3fab8e0e30b2aa58305e7e5dbbefb69be3a22583
Sampo Virmasalo <sampo.virmasalo@meiko.fi>
2023-04-11 15:42:09 +0300

    Fix breadcrumb path
    prevents /city/city from happening

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,20 +1,20 @@
 <template>
     <ol class="sr-only" itemscope itemtype="https://schema.org/BreadcrumbList">
         <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-            <NuxtLink to="/" itemprop="item" itemscope itemtype="https://schema.org/WebPage" itemid="https://koirametsat.info">
+            <NuxtLink to="/" itemprop="item" itemscope itemtype="https://schema.org/WebPage" itemid="https://koirametsat.info" noPrefetch>
                 <span itemprop="name">koirametsat.info</span>
             </NuxtLink>
             <meta itemprop="position" content="1" />
         </li>
-        <li v-for="(item, index) in pathItems.slice(1)" :key="index" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-            <NuxtLink :to="`${item}`" itemprop="item" itemscope itemtype="https://schema.org/WebPage" :itemid="`https://koirametsat.info/${item}`">
-                <span itemprop="name">{{ item }}</span>
+        <li v-for="(pi, index) in pathItems" :key="pi" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+            <NuxtLink :to="`/${pi}`" itemprop="item" itemscope itemtype="https://schema.org/WebPage" :itemid="`https://koirametsat.info/${pi}`" noPrefetch>
+                <span itemprop="name">{{ pi }}</span>
             </NuxtLink>
             <meta itemprop="position" :content="`${index + 2}`" />
         </li>
         <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
                 <span itemprop="name">{{ useParseTitle(curPageTitle) }}</span>
-            <meta itemprop="position" :content="`${pathItems.length + 1}`" />
+            <meta itemprop="position" :content="`${pathItemsTotal}`" />
         </li>
     </ol>
 </template>
@@ -24,6 +24,9 @@ const props = defineProps({
   curPageTitle: { type: String, default: "" }
 });
 const pathItems = props.path.split("/");
+const pathItemsTotal = pathItems.length;
+// Substract the first item from the array
+pathItems.shift();
 // Substract the last item from the array
 pathItems.pop();
 </script>
