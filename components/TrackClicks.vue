168ca965481b30b0695ec8c42e6696e45e3f6cf5
Sampo Virmasalo <sampo.virmasalo@meiko.fi>
2023-08-04 15:19:53 +0300

    Track clicks & kaupungit serp

---
+++ components/TrackClicks.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const links = document.getElementsByTagName('a');
+    const trackClick = (e) => {
+        const link = e.target;
+        const trackId = link.dataset.trackId;
+        const href = link.href;
+
+        try {
+            dataLayer.push({
+                event: "Item click",
+                to: href,
+                view: document.title,
+                itemId: trackId,
+            });
+        } catch (error) {
+            console.warn("Datalayer not defined");
+        }
+    }
+    if (links) {
+        for (let i = 0; i < links.length; i++) {
+            links[i].setAttribute('data-track-id', i);
+            links[i].addEventListener('click', trackClick);
+        }
+    }
+</script>
+<template>
+    <!-- Just for the lulz -->
+    <p class="sr-only"></p>
+</template>
